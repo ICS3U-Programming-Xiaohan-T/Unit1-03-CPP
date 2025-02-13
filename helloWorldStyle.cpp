8a3a9e95d9a4b8b3aa607be519a33a24eaada943
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-02-13 05:54:58 +0000

    Completed style check and added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright • (c) 2025 Xiaohan. All rights reserved. Created by: Xiaohan.
+// Date: Feb. 12, 2025
+// This program displays "Hello, World with style."
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World with style!" << std::endl;
+  std::cout << "C++ is a bit more complicated than Python." << std::endl;
+    return 0;
+}
